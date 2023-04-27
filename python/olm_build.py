4b69958c955758a5d87412b6f23bd85018b25580
Hubert Chathi <hubertc@matrix.org>
2023-04-27 18:52:39 -0400

    improve compatibility with Windows (though it still doesn't work)

--- python/olm_build.py
+++ python/olm_build.py
@@ -26,7 +26,6 @@ PATH = os.path.dirname(__file__)
 DEVELOP = os.environ.get("DEVELOP")
 
 compile_args = ["-I../include"]
-link_args = ["-L../build"]
 
 if DEVELOP and DEVELOP.lower() in ["yes", "true", "1"]:
     link_args.append('-Wl,-rpath=../build')
@@ -44,8 +43,8 @@ ffibuilder.set_source(
         #include <olm/sas.h>
     """,
     libraries=["olm"],
+    library_dirs=[os.path.join("..", "build")],
     extra_compile_args=compile_args,
-    extra_link_args=link_args,
     source_extension=".cpp", # we need to link the C++ standard library, so use a C++ extension
 )
 
