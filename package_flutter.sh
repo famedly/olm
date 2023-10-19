2351466d72bcc0532f20845649c42e2bdc3f9c9e
Nicolas Werner <n.werner@famedly.com>
2023-10-19 14:02:55 +0200

    chore: add github workflows

--- package_flutter.sh
+++ package_flutter.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -e
-rm -rf build/flutter
-mkdir -p build
-cp -rL flutter build/flutter
+rm -rf publishing/flutter
+mkdir -p publishing
+cp -rL flutter publishing/flutter
 
 sed "$(for i in MAJOR MINOR PATCH; do
 v=$(sed -n '/'$i' *=/{s/^[^=]*=//;/^[0-9 ]*$/p;q;}' OLMKit.podspec)
 [ -n "$v" ] && echo 's/^\([ \t]*'$i' *=\).*$/\1'"$v/"
-done)" <flutter/ios/flutter_olm.podspec >build/flutter/ios/flutter_olm.podspec
+done)" <flutter/ios/flutter_olm.podspec >publishing/flutter/ios/flutter_olm.podspec
 
-sed '/^publish_to: none/d' <flutter/pubspec.yaml >build/flutter/pubspec.yaml
+sed '/^publish_to: none/d' <flutter/pubspec.yaml >publishing/flutter/pubspec.yaml
